9fcaa88b1ffd44955ad82c16c6be27023d09c24d
olegios <oleg.saloied@gmail.com>
2015-08-18 23:45:29 +0300

    Create mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDir>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonLoad_clicked();
+    void on_pushButtonNew_clicked();
+    void slotOpen();
+
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
+
